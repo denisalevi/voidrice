a2d953632f399399e107be8cfc91a2e3094b90f4
Denis Alevi <mail@denisalevi.de>
2026-04-17 10:26:28 +0200

    Switch default external USB-C port

--- .screenlayout/work.sh
+++ .screenlayout/work.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sh ~/.screenlayout/_only-external-work.sh
 sleep 0.1
-xrandr --output eDP-1 --mode 1920x1200 --pos 3840x1390 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-4 --off
+xrandr --output eDP-1 --mode 1920x1200 --pos 3840x1390 --rotate normal --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-4 --off
 scaledpi 4
 sleep 0.1
 ~/.local/bin/remaps

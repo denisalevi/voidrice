9ae9fe8c112d1a7538e0a7c9d996a73e19eab4ec
Denis Alevi <mail@denisalevi.de>
2026-07-01 09:37:56 +0200

    Add screenlayout configs

---
+++ .screenlayout/touch-right-portrait.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal \
+       --output HDMI-1 --off \
+       --output DP-1 --mode 1920x1200 --pos 1920x0 --rotate right \
+       --output DP-2 --off \
+       --output DP-3 --off \
+       --output DP-4 --off
+scaledpi 2
+
+touch_device='Silicon Integrated System Co. SiS HID Touch Controller'
+
+xinput map-to-output "$touch_device" DP-1
